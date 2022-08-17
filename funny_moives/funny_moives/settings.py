87eec4a487af45b61226eb1498c7715cff08661c
ThinhNgV <nguyen-van-thinh@jcm-hq.co.jp>
2022-08-17 18:30:06 +0900

    Remove a hardcoded Django secret key

--- funny_moives/funny_moives/settings.py
+++ funny_moives/funny_moives/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-i=-xj8cs!l1u=l0!p_7dst18v--25qc1i+q4n$)ytetd_j^_7b'
+# SECRET_KEY = 'django-insecure-i=-xj8cs!l1u=l0!p_7dst18v--25qc1i+q4n$)ytetd_j^_7b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
